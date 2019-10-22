e6f70ba6e7e9a0e5e24722d9dbd07316e12d54bb
Nicolas Schejtman <nschejtman93@gmail.com>
2019-10-22 10:26:17 -0300

    Add expansion with @value for arrays in expander

--- expander-expanded.js
+++ expander-expanded.js
@@ -1,38 +1,49 @@
 exports.expander = class JsonLdExpander {
     // Expands jsonld if flattened and returns a native js object. If it is already expanded, it just parses and returns the js object.
     static expand(flattened) {
-        const f = JSON.parse(flattened);
-        const graph = f['@graph'];
+        const f = JSON.parse(flattened)
+        const graph = f['@graph']
         if (graph) {
-            const cache = {};
-            JsonLdExpander.populateCache(graph, cache);
+            const cache = {}
+            JsonLdExpander.populateCache(graph, cache)
             for (let i = graph.length - 1; i >= 0; i--) {
-                const current = graph[i];
-                const id = current['@id'];
-                cache[id] = current;
+                const current = graph[i]
+                const id = current['@id']
+                cache[id] = current
                 JsonLdExpander.traverse(current, cache)
             }
             return JsonLdExpander.normalize(graph)
         } else return f
     }
+
     static traverse(element, cache) {
         Object.keys(element).forEach(key => {
-            const value = element[key];
+            const value = element[key]
             if (typeof value === 'object' && value !== null && !key.endsWith("link-target") && !key.endsWith("reference-id") && !key.endsWith("fixPoint")) {
-                const isArray = Array.isArray(value);
+                const isArray = Array.isArray(value)
                 if (isArray) {
-                    JsonLdExpander.replaceIdReferencesInArray(element, key, cache)
+                    if (this.isArrayOfLinks(value)) {
+                        JsonLdExpander.replaceIdReferencesInArray(element, key, cache)
+                    } else if (key !== "@type") {
+                        element[key] = element[key].map(value => this.normalizeValue(value))
+                    }
+
                 } else {
                     JsonLdExpander.replaceIdReference(element, key, cache)
                 }
             } else if((typeof value === 'string' || typeof value === 'number' || typeof value === 'boolean') && key !== '@id') {
-                element[key] = [{'@value': value}]
+                element[key] = [this.normalizeValue(value)]
             }
         })
     }
+
+    static normalizeValue(value) {
+        return {"@value": value}
+    }
+
     static populateCache(graph, cache) {
         graph.forEach(element => {
-            const id = element['@id'];
+            const id = element['@id']
             cache[id] = element
         })
     }
@@ -40,17 +51,17 @@ exports.expander = class JsonLdExpander {
         return [graph[0]]
     }
     static replaceIdReference(element, key, cache) {
-        const linkObject = element[key];
-        const retrievedTarget = JsonLdExpander.retrieveIfIsLink(linkObject, cache);
+        const linkObject = element[key]
+        const retrievedTarget = JsonLdExpander.retrieveIfIsLink(linkObject, cache)
         element[key] = [retrievedTarget]
     }
     static replaceIdReferencesInArray(element, key, cache) {
-        const links = element[key];
+        const links = element[key]
         element[key] = links.map(linkObject => JsonLdExpander.retrieveIfIsLink(linkObject, cache))
     }
     static retrieveIfIsLink(element, cache) {
         if (JsonLdExpander.isLinkObject(element)) {
-            const retrievedTarget = JsonLdExpander.retrieveLinkTargetFrom(element, cache);
+            const retrievedTarget = JsonLdExpander.retrieveLinkTargetFrom(element, cache)
             if (retrievedTarget) {
                 return retrievedTarget
             } else {
@@ -58,10 +69,10 @@ exports.expander = class JsonLdExpander {
             }
         } else if(element['http://a.ml/vocabularies/document-source-maps#element'] && element['http://a.ml/vocabularies/document-source-maps#value']){
             // annotations expansion to @value
-            const elementKey = 'http://a.ml/vocabularies/document-source-maps#element';
-            const valueKey = 'http://a.ml/vocabularies/document-source-maps#value';
-            element[elementKey] = [{'@value': element[elementKey]}];
-            element[valueKey] = [{'@value': element[valueKey]}];
+            const elementKey = 'http://a.ml/vocabularies/document-source-maps#element'
+            const valueKey = 'http://a.ml/vocabularies/document-source-maps#value'
+            element[elementKey] = [{'@value': element[elementKey]}]
+            element[valueKey] = [{'@value': element[valueKey]}]
             return element
         } else {
             return element
@@ -70,12 +81,17 @@ exports.expander = class JsonLdExpander {
     static isLinkObject(element) {
         return element['@id'] !== undefined && Object.keys(element).length === 1
     }
+
+    static isArrayOfLinks(array) {
+        return array.every(element => this.isLinkObject(element));
+    }
+
     static retrieveLinkTargetFrom(linkObject, cache) {
-        const id = linkObject['@id'];
-        const target = cache[id];
+        const id = linkObject['@id']
+        const target = cache[id]
         if (target === undefined) {
             console.log("cache miss " + id)
         }
         return target
     }
-};
+}
